0517073f0629764341b8334f400f474aa81c1100
JHerrera5 <jherrera@existaya.com>
2022-10-25 15:38:11 -0500

    se prepara el proyecto

---
+++ sistemaventas/configuracion/global.php
@@ -0,0 +1,16 @@
+<?php
+
+
+define("BD HOST", "localhost");
+
+define("BD_NAME", "dbsistema");
+
+define("DB_USERNAME","root");
+
+define("DB_PASSWORD","");
+
+define("DB_ENCODE","uft8");
+
+define("Sistema_nombre","pos_system");
+
+?>
